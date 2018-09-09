daa4f5639e3d38337f1b7cccd7fdd1ea786784f5
joanlamrack <joanlamrack@gmail.com>
2018-09-10 01:12:27 +0700

    added More login providers

--- src/configs/firebaseUI.js
+++ src/configs/firebaseUI.js
@@ -6,10 +6,11 @@ import {auth} from "./firebase";
 export const uiConfig = {
 	signInFlow:"popup",
 	signInOptions:[
-		firebase.auth.GoogleAuthProvider.PROVIDER_ID,
 		firebase.auth.EmailAuthProvider.PROVIDER_ID,
+		firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+		firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+		firebase.auth.TwitterAuthProvider.PROVIDER_ID,
 		firebase.auth.GithubAuthProvider.PROVIDER_ID,
-		firebase.auth.FacebookAuthProvider.PROVIDER_ID
 	]
 };
 
